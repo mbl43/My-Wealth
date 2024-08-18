8e6154a4fd12387b0c66711cf968c0ef8f9dcee3
mangesh lemte <mangeshlemtea23@gmail.com>
2024-08-18 22:25:07 +0530

    node module,tailwind installed and project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-4xl w-full h-screen flex justify-center mx-auto items-center'>
+      My Wealth
+    </div>
+  )
+}
+
+export default App
